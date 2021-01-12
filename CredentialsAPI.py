dcfa5d9b50b194d4cff710ef906840848ad556c8
Santiago Linares <Santiago@MacBook-Pro-de-Santiago-930.local>
2021-01-12 19:20:38 +0100

    Remove Credentials file

--- CredentialsAPI.py
+++
@@ -1,5 +0,0 @@
-client_id = 'SkI4ODVWMGFWWWlXV2VreUVLQ2tpdzpiNWU1NGQ1ZjA2NTI3NDRh' #personal client ID for Mapillary API
-token = 'xMkJ8SMaDe6lz3HHaQuWmRvcnAANXn8PneCeJQd9d1qKIyO6t10ygBnrLdNGA5k4flc0w4yhTGieBihrN9vebqtWxFPazldBaGGirB1ONYtm0iXP0kaYcTxhKHmpbMCCXqKVUqv6u0ArDYLMigVtVJev64e3oXh7' #Personal token for Mapillary API
-
-access_key = 'AKIAZRVJYWVSBO3L6QHU' # access_key for S3 AWS 
-secret_access_key = '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' # secret_access_key for S3 AWS 
