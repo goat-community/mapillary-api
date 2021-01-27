09c0fd1640028ad08f3b99498829c0da3e635cc4
gregoriiv <grigorii.ivanov@tum.de>
2021-01-27 22:55:59 +0100

    Functions to get sets of objects and features + config

--- CallsApi.py
+++ CallsApi.py
@@ -1,15 +1,16 @@
-from MapFeatures import GetFeatures
-from MapSegmentations import Object_Detection
-from CredentialsApi import *
+import MapFeaturesFunctions
+import MapObjectsFunctions
+from CredentialsAPI import client_id, token
+import os
 
-bbox = '11.5486908, 48.1582993, 11.5747833, 48.1404324' #minlon, maxlat, maxlon, minlat
-values = 'object--vehicle--car' # reference values you'd like here, separate multiple by comma, leave empty quotes to get all values
-max_score = '0.1' #Filter detections with the maximal score (Percentage of value within the image).
-min_score = '0' #Filter detections with the minimal score.
-layers = 'segmentations' # choose trafficsigns or segmentations or instances
-filename = 'car-0to100' #filename without extension (extension will be geojosn)
 
-#filename = 'TUM-trash-can'
+fact = 0.035
+min_score = 0.7
 
-GetFeatures(bbox,values,client_id,token,layers,filename)
-Object_Detection(bbox, values, client_id, max_score, min_score, token, layers,filename)
+#path = 'F:\Hasenbergl\goat\\app\database\data\study_area'
+path = 'data\\study_area\\test_st_ar'
+#path = 'F:\Privat\Study\TUM\Environmental_Engineering_18.19\WiSe2021\MasterT\GOAT_SPGG\goat\\app\database\data\study_area'
+
+#MapFeaturesFunctions.MapillaryMultiFeaturesRequest(path, fact, client_id, token)
+
+MapObjectsFunctions.MapillaryMultiObjectsRequest(path, fact, client_id, min_score, token)
